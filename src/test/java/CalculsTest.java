f22267e63fc3505102e8b8bc0a33a8d99c35feb6
mthfr <matahi.fareea@epsi.fr>
2020-12-14 17:05:55 +0100

    add suresfire

--- src/test/java/CalculsTest.java
+++ src/test/java/CalculsTest.java
@@ -13,7 +13,7 @@ class CalculsTest {
      */
     @BeforeEach
     void setUp() throws Exception {
-        c1 = new Calculs(1,2);
+        c1 = new Calculs(10,2);
         c2 = new Calculs(10,20);
         c3 = new Calculs(100,200);
     }
@@ -22,31 +22,22 @@ class CalculsTest {
      * Test method for {@link Calculs#multiplier()}.
      */
     @Test
-    void multiplier() {
-        if(c1.multiplier() != 2){
+    void testMultiplier() {
+        if(c1.multiplier() == 2){
             fail("Methode multiplier non conforme 2*1=2 ne fonctionne pas.");
         }
-        assertEquals(c1.multiplier(),42);
+        assertEquals(c1.multiplier(),20);
     }
 
-    /**
-     * Test method for {@link Calculs#additionner()}.
-     */
     @Test
-    void additionner() {
+    void testAdditionner() {
     }
 
-    /**
-     * Test method for {@link Calculs#diviser()}.
-     */
     @Test
-    void diviser() {
+    void testDiviser() {
     }
 
-    /**
-     * Test method for {@link Calculs#soustraire()}.
-     */
     @Test
-    void soustraire() {
+    void testSoustraire() {
     }
 }
